deffcc063636918964d7c384dfe282c9c5b801c7
Senik Hakobyan <sen.hakobyan@gmail.com>
2020-12-19 00:02:41 +0400

    Integration test for PrimeNumber service

---
+++ tests/Integration/Services/PrimeNumberTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Integration\Services;
+
+use App\Models\RequestedNumber;
+use App\Repositories\RequestedNumbersRepository;
+use App\Services\PrimeNumber;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PrimeNumberTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private PrimeNumber $service;
+    private RequestedNumbersRepository $repo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repo    = new RequestedNumbersRepository(new RequestedNumber());
+        $this->service = new PrimeNumber($this->repo);
+    }
+
+    public function testStore()
+    {
+        $number   = 7;
+        $response = $this->service->store($number);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Yes, {$number} IS a prime number!", $response->getMessage());
+        $this->assertEquals(1, $response->getData()['count']);
+
+        $response = $this->service->store($number);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("YES, and we already told you so!", $response->getMessage());
+        $this->assertEquals(2, $response->getData()['count']);
+
+        for($i = 0; $i <= 8; $i++)
+        {
+            $response = $this->service->store($number);
+        }
+
+        $this->assertEquals(403, $response->getStatusCode());
+        $this->assertEquals("You're insane, we don't want to answer anymore.", $response->getMessage());
+        $this->assertEquals(11, $response->getData()['count']);
+    }
+}
